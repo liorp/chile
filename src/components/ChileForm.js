164462f250bc55504b79687ef9fe0476a65ddf7c
Eliran <eliran.ag93@gmail.com>
2020-03-30 13:54:51 +0300

    refactor chile form class to function

--- src/components/ChileForm.js
+++ src/components/ChileForm.js
@@ -1,36 +1,53 @@
-import React, { Component } from 'react'
-import ChileArg from './ChileArg'
+import React, { useState, useEffect } from 'react';
+import { Button } from '@material-ui/core';
+import ChileArg from './ChileArg';
 
-class ChileForm extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = this.props.args.reduce((state, arg) => {
-            state[[arg.name]] = arg.value ?? null;
-            return state;
-        }, {});
-    }
+function ChileForm(props) {
+    console.log('renderrrr')
 
-    getArgs = () => this.props.args.map((arg, i) =>
+    const initialArgs = props.args.reduce((state, arg) => {
+        state[[arg.name]] = arg.value ?? null;
+        return state;
+    }, {});
+    const [args, setArgs] = useState(initialArgs);
+
+    useEffect(() => props.onFormUpdate({ ...args }));
+
+    const getChileArgs = () => props.args.map((arg, i) =>
         <ChileArg key={i}
             name={arg.name}
             nickname={arg.nickname}
             type={arg.type}
-            value={this.state[[arg.name]]}
+            value={args[[arg.name]]}
             enabled={arg.enabled}
             required={arg.required}
             options={arg.options}
-            onValueChange={val => this.setState({ [arg.name]: val },
-                () => this.props.onFormUpdate({ ...this.state }))} />
+            onValueChange={val => setArgs({ ...args, [arg.name]: val }) } />
     );
 
-    render() {
-        return (
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-                {this.getArgs()}
-            </div>
-        );
-    }
+    const getCompleteButton = () => {
+        const { completeButton, onComplete } = props;
+        return completeButton
+            ? <Button variant="outlined"
+                      disabled={!isValid()}
+                      onClick={() => onComplete(args)}>
+                    {completeButton}
+              </Button>
+            : '';
+    };
+
+    const isValid = () => {
+        return props.args.filter(a => a.required &&
+            (args[[a.name]] == null || args[[a.name]] === '')).length == 0;
+    };
+
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+            {getChileArgs()}
+            {getCompleteButton()}
+        </div>
+    );
 }
 
 export default ChileForm;
